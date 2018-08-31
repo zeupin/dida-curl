6d3c21f8a896bf653626b2aba7d401f0366a902f
Macc Liu <maccliu@qq.com>
2018-09-01 02:09:06 +0800

    * 改进了request()的处理逻辑 * 新增postjson()快捷方法

--- src/Dida/CURL/CURL.php
+++ src/Dida/CURL/CURL.php
@@ -11,17 +11,35 @@ namespace Dida\CURL;
 
 class CURL
 {
-    const VERSION = '20180427';
+    const VERSION = '20180901';
 
     const ERR_INVALID_METHOD = -1;
+
+    public $method = "GET";
+    public $valid_methods = ["GET", "POST"];
+
+    public $header = [];
+
+
+    public function addHeader($line)
+    {
+        $this->header[] = $line;
+    }
+
+
+    public function clearHeaders()
+    {
+        $this->header = [];
+    }
+
 
     public function request(array $input, array $curloptions = [])
     {
         $url = $input["url"];
 
-        $method = (isset($input["method"])) ? $input["method"] : "GET";
+        $method = (isset($input["method"])) ? $input["method"] : $this->method;
         $method = strtoupper($method);
-        if (!in_array($method, ["GET", "POST"])) {
+        if (!in_array($method, $this->valid_methods)) {
             return [self::ERR_INVALID_METHOD, "无效的请求方式", null];
         }
 
@@ -67,7 +85,21 @@ class CURL
             CURLOPT_HEADER         => 0,
             CURLOPT_RETURNTRANSFER => 1,
         ];
+
         curl_setopt_array($curl, $defaults);
+
+        $header = $this->header;
+        if (array_key_exists(CURLOPT_HTTPHEADER, $curloptions)) {
+            $header = array_merge($header, $curloptions[CURLOPT_HTTPHEADER]);
+            $header = array_unique($header);
+            unset($curloptions[CURLOPT_HTTPHEADER]);
+        }
+        if ($header) {
+            curl_setopt_array($curl, [
+                CURLOPT_HEADER     => 1,
+                CURLOPT_HTTPHEADER => $header
+            ]);
+        }
 
         curl_setopt_array($curl, $curloptions);
 
@@ -81,5 +113,60 @@ class CURL
         curl_close($curl);
 
         return [0, null, $data];
+    }
+
+
+    public function postjson($url, $json)
+    {
+        $input = [
+            "url"    => $url,
+            "method" => "POST",
+            "data"   => $json,
+        ];
+
+        $curloptions = [
+            CURLOPT_HEADER     => 1,
+            CURLOPT_HTTPHEADER => [
+                "Content-Type: application/json;charset=UTF-8"
+            ],
+        ];
+
+        return $this->request($input, $curloptions);
+    }
+
+
+    public function parseHttpResponse($resp)
+    {
+        $matches = null;
+
+        $lines = explode("\r\n", $resp);
+
+        $line1 = $lines[0];
+        $r = preg_match("/(HTTP\/)(\d\.\d)\s(\d\d\d)/", $line1, $matches);
+        if ($r) {
+            $statusCode = $matches[3];
+        } else {
+            return false;
+        }
+        unset($lines[0]);
+
+        $headers = [];
+        foreach ($lines as $n => $line) {
+            if ($line === '') {
+                unset($lines[$n]);
+                break;
+            }
+
+            $headers[] = $line;
+            unset($lines[$n]);
+        }
+
+        $body = implode("\r\n", $lines);
+
+        return [
+            "code"    => $statusCode,
+            "headers" => $headers,
+            "body"    => $body,
+        ];
     }
 }
